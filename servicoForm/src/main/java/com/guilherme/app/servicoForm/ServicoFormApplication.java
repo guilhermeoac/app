1805348fe9727db6ff4fd9310442450d2d547ef7
guilherme <guilhermeoliveiracosta@hotmail.com>
2020-05-02 13:49:37 -0300

    Commit inicial

---
+++ servicoForm/src/main/java/com/guilherme/app/servicoForm/ServicoFormApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.app.servicoForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoFormApplication.class, args);
+	}
+
+}
---
+++ servicoForm/src/test/java/com/guilherme/app/servicoForm/ServicoFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.app.servicoForm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
